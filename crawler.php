8183a8964ce68afe4a3018e246fb815c294c1583
Arijeet Baruah <arijeet.webointern@gmail.com>
2016-09-14 15:44:43 -0700

    session2 first commit

--- crawler.php
+++ crawler.php
@@ -4,6 +4,12 @@ function get_link1($target_url){
     $html = new simple_html_dom();
     $html->load_file($target_url);
     $links=$html->find('a');
+    $base_url=$target_url;
+    if(substr($base_url, 0, 7)=='http://'){
+        $base_url=substr($base_url, 7);
+    }else if(substr($base_url, 0, 8)=='https://'){
+        $base_url=substr($base_url, 8);
+    }
     foreach($links as $link){
         if(strpos($link->href,'#')){
             $link->href=substr($link->href,0,strpos($link->href,'#'));
@@ -15,8 +21,12 @@ function get_link1($target_url){
             $link->href=$target_url;
         }else if(substr($link->href,0,7)=='mailto:'){
             $link->href="[$link->href]";
+        }else if(substr($link->href,0,2)=='//'){
+            $link->href=substr($link->href,2);
+        }else if(substr($link->href,0,1)=='/'){
+            $link->href=''.$base_url.$link->href;
         }
-        printf ("<li>$link->href</li>");
+        echo ("<li>$link->href</li>");
     }
 }
 if(isset($_REQUEST["url"])){
