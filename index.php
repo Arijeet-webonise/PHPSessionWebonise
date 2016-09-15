35a0ca7c5604ec893b35d011ae5023b248b2a3ab
Arijeet Baruah <arijeet.webointern@gmail.com>
2016-09-15 04:26:53 -0700

    opps concept add

--- index.php
+++ index.php
@@ -1,43 +1,78 @@
 <?php
 	session_start();
+	/**
+	* 
+	*/
 	$text='LEGEND';
 	$vowel=array('A','E','I','O','U');
-	if(isset($_REQUEST['tries'])){
-		$tries=$_REQUEST['tries'];
-		if(2==$tries){
-			ob_start();
-		    header('refresh:5;url=gameover.php');
-		    ob_end_flush();
-		}
-	}else{
-		$tries=7;
-	}
 	$sample=array();
-	for ($i=0;$i<strlen($text);$i++) {
-		array_push($sample,$text[$i]);
-	}
-	if(isset($_SESSION['word']))
-		$textarray=$_SESSION['word'];
-	else{
-		$textarray=$sample;
-		foreach ($textarray as $key => $letter) {
-			if(!in_array($letter,$vowel)){
-				$textarray[$key]='_';
-			}
+	class HangMan
+	{
+		public $textarray=NULL;
+		private $text;
+		private $vowel;
+		public $sample;
+		function HangMan(){		
 		}
-	}
-	if(isset($_REQUEST['letter'])){
-		$letter=$_REQUEST['letter'];
-		if(in_array($letter,$sample)){
-			foreach ($sample as $key => $value) {
-				if($value==$letter){
-					$textarray[$key]=$letter;
+
+		function counttries(){
+			if(!isset($_REQUEST['tries'])){
+				return 7;
+			}
+			$tries=$_REQUEST['tries'];
+			if(2==$tries){
+				ob_start();
+			    header('refresh:5;url=gameover.php');
+			    ob_end_flush();
+			}
+			return $tries;
+		}
+
+		function check($tries,$text,$vowel,$sample){
+			$t=array('text'=>$sample,'tries'=>$tries);
+			for ($i=0;$i<strlen ($text);$i++) {
+				array_push($sample,$text[$i]);
+			}
+
+			if(isset($_SESSION['word'])){
+				$textarray=$_SESSION['word'];
+			}
+			else{
+				$textarray=$sample;
+				foreach ($textarray as $key => $letter) {
+					if(!in_array($letter,$vowel)){
+						$textarray[$key]='_';
+					}
 				}
 			}
-		}else{
-			$tries--;
+
+			if(isset($_REQUEST['letter'])){
+				$letter=$_REQUEST['letter'];
+				if(in_array($letter,$sample)){
+					foreach ($sample as $key => $value) {
+						if($value==$letter){
+							$textarray[$key]=$letter;
+						}
+					}
+				}else{
+					$tries--;
+				}
+			}
+			// var_dump(in_array('_',$textarray));
+			if(!in_array('_',$textarray)){
+				header('refresh:5;url=victory.php');
+			}
+			$t['tries']=$tries;
+			$t['text']=$textarray;
+			return $t;
 		}
 	}
+
+	$hm=new HangMan();
+	$tries=$hm->counttries();
+	$array=$hm->check($tries,$text,$vowel,$sample);
+	$textarray=$array['text'];
+	$tries=$array['tries'];
 	$_SESSION['word']=$textarray;
 ?>
 <!DOCTYPE html>
