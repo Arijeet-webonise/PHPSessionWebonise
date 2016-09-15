9304897e895bbafa7cd257007e213c760073f46b
Arijeet Baruah <arijeet.webointern@gmail.com>
2016-09-14 17:03:44 -0700

    update:

--- crawler.php
+++ crawler.php
@@ -7,8 +7,10 @@ function get_link1($target_url){
     $base_url=$target_url;
     if(substr($base_url, 0, 7)=='http://'){
         $base_url=substr($base_url, 7);
+        $base_url='http://'.substr($base_url, 0, strpos($base_url, '/'));
     }else if(substr($base_url, 0, 8)=='https://'){
         $base_url=substr($base_url, 8);
+        $base_url='https://'.substr($base_url, 0, strpos($base_url, '/'));
     }
     foreach($links as $link){
         if(strpos($link->href,'#')){
@@ -22,9 +24,9 @@ function get_link1($target_url){
         }else if(substr($link->href,0,7)=='mailto:'){
             $link->href="[$link->href]";
         }else if(substr($link->href,0,2)=='//'){
-            $link->href=substr($link->href,2);
+            $link->href=$base_url.substr($link->href,2);
         }else if(substr($link->href,0,1)=='/'){
-            $link->href=''.$base_url.$link->href;
+            $link->href=$base_url.$link->href;
         }
         echo ("<li><a href='$link->href'>$link->href</a></li>");
     }
