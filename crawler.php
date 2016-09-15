4a104dbdddae42e9c39f741262a1d6b1dc6ab85f
Arijeet Baruah <arijeet.webointern@gmail.com>
2016-09-15 10:26:26 -0700

    Task Complete

--- crawler.php
+++ crawler.php
@@ -1,8 +1,15 @@
 <?php
 require_once('simple_html_dom/simple_html_dom.php');
-function get_link1($target_url){
+function get_link1($target_url,$num){
     $html = new simple_html_dom();
     $html->load_file($target_url);
+    $title=$html->find('title');
+    $img=$html->find('img');
+    echo '<h3>Title '.($title[0]->nodes[0]->_['4']).'</h3>';
+    foreach ($img as $key) {
+         # code...
+        echo $key;
+    }
     $links=$html->find('a');
     $base_url=$target_url;
     if(substr($base_url, 0, 7)=='http://'){
@@ -12,6 +19,7 @@ function get_link1($target_url){
         $base_url=substr($base_url, 8);
         $base_url='https://'.substr($base_url, 0, strpos($base_url, '/'));
     }
+    echo '<ul>';
     foreach($links as $link){
         if(strpos($link->href,'#')){
             $link->href=substr($link->href,0,strpos($link->href,'#'));
@@ -26,12 +34,18 @@ function get_link1($target_url){
         }else if(substr($link->href,0,2)=='//'){
             $link->href=$base_url.substr($link->href,2);
         }else if(substr($link->href,0,1)=='/'){
-            $link->href=$base_url.$link->href;
+            $link->href=$link->href;
+        }else if($link->href=="javascript:void(0);"){
+            continue;
         }
         echo ("<li><a href='$link->href'>$link->href</a></li>");
+        if($num>0){
+            get_link1($link->href,$num-1);
+        }
     }
+    echo '</ul>';
 }
 if(isset($_REQUEST["url"])){
-    get_link1($_REQUEST["url"]);
+    get_link1($_REQUEST["url"],3);
 }
 ?>
