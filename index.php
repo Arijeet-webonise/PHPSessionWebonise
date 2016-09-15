d40728d000a66a403c1fccce94ca5c888e5f0908
Arijeet Baruah <arijeet.webointern@gmail.com>
2016-09-15 05:16:41 -0700

    task complete

--- index.php
+++ index.php
@@ -1,85 +1,74 @@
-<?php
-	session_start();
-	/**
-	* 
-	*/
-	$vowel=array('A','E','I','O','U');
-	$try="tries";
-	$text='LEGEND';	
-	$sample=array();
-	class HangMan
-	{
-		public $textarray=NULL;
-		private $text;
-		private $vowel;
-		public $tries;
-		public $sample;
-		function HangMan(){	
-			// $text='LEGEND';	
-		}
+<?php 
+session_start();
+class HangMan
+{
+	public $vowel=array('A','E','I','O','U');
+	public $text='LEGEND';	
+	public $sample=array();
+    public $tries;
+    public $textarray;
 
-		function counttries(){
-			if(!isset($_REQUEST['tries'])){
-				return 7;
-			}
-			$tries=$_REQUEST['tries'];
-			if(2==$tries){
+    function gettextarray(){
+    	return $this->textarray;
+    }
+    function gettries(){
+    	return $this->tries;
+    }
+    function counttries(){
+		if(!isset($_REQUEST['tries'])){
+			$this->tries= 7;
+		}else{
+			$this->tries=$_REQUEST['tries'];
+			if(2==$this->tries){
 				ob_start();
 			    header('refresh:5;url=gameover.php');
 			    ob_end_flush();
 			}
-			$try="tries";
-			return $tries;
-		}
-
-		function check($tries,$text,$vowel,$sample){
-			$try="tries";
-			$t=array('text'=>$sample,'tries'=>$tries);
-			for ($i=0;$i<strlen ($text);$i++) {
-				array_push($sample,$text[$i]);
-			}
-
-			if(isset($_SESSION['word'])){
-				$textarray=$_SESSION['word'];
-			}
-			else{
-				$textarray=$sample;
-				foreach ($textarray as $key => $letter) {
-					if(!in_array($letter,$vowel)){
-						$textarray[$key]='_';
-					}
-				}
-			}
-
-			if(isset($_REQUEST['letter'])){
-				$letter=strtoupper($_REQUEST['letter']);
-				if(in_array($letter,$sample)){
-					foreach ($sample as $key => $value) {
-						if($value==$letter){
-							$textarray[$key]=$letter;
-						}
-					}
-				}else{
-					$tries--;
-				}
-			}
-			// var_dump(in_array('_',$textarray));
-			if(!in_array('_',$textarray)){
-				header('refresh:5;url=victory.php');
-			}
-			$t['text']=$textarray;
-			$t['tries']=$tries;
-			return $t;
 		}
 	}
 
-	$hm=new HangMan();
-	$tries=$hm->counttries();
-	$array=$hm->check($tries,$text,$vowel,$sample);
-	$textarray=$array['text'];
-	$tries=$array['tries'];
-	$_SESSION['word']=$textarray;
-?>
+	function check(){
+		$this->try="tries";
+		for ($i=0;$i<strlen ($this->text);$i++) {
+			array_push($this->sample,$this->text[$i]);
+		}
+
+		if(isset($_SESSION['word'])){
+			$this->textarray=$_SESSION['word'];
+		}
+		else{
+			$this->textarray=$this->sample;
+			foreach ($this->textarray as $key => $letter) {
+				if(!in_array($letter,$this->vowel)){
+					$this->textarray[$key]='_';
+				}
+			}
+		}
+
+		if(isset($_REQUEST['letter'])){
+			$letter=strtoupper($_REQUEST['letter']);
+			if(in_array($letter,$this->sample)){
+				foreach ($this->sample as $key => $value) {
+					if($value==$letter){
+						$this->textarray[$key]=$letter;
+					}
+				}
+			}else{
+				$this->tries--;
+			}
+		}
+		// var_dump(in_array('_',$textarray));
+		if(!in_array('_',$this->textarray)){
+			header('refresh:5;url=victory.php');
+		}
+	}
+}
+
+$hm = new HangMan();
+$hm->counttries();
+$hm->check();
+$_SESSION['word']=$hm->gettextarray();
+?> 
 <!DOCTYPE html>
 <html>
 <head>
@@ -89,14 +78,14 @@
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div class="well" id="Text"><?= implode(' ',$textarray) ?></div>
+	<div class="well" id="Text"><?= implode(' ',$hm->gettextarray()) ?></div>
 	<div class="col-sm-6">
 		<form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-			<input type="text" name="letter" <?php if($tries==1){?> disabled <?php } ?>>
+			<input type="text" name="letter" <?php if($hm->gettries()==1){?> disabled <?php } ?>>
 			<input type="hidden" name="tries" value="<?php $try="tries";
-			 echo $tries; ?>">
+			 echo $hm->gettries(); ?>">
 		</form>
 	</div>
-	<div class="col-sm-6" id="hangman"><img src="Assets/try<?php if($tries>0){echo 8-$tries;}else{echo 7;} ?>.gif"></div>
+	<div class="col-sm-6" id="hangman"><img src="Assets/try<?php if($hm->gettries()>0){echo 8-$hm->gettries();}else{echo 7;} ?>.gif"></div>
 </body>
 </html>
