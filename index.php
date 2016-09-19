80d687e0e709abe0b8899297aa21fd674152adc1
Arijeet Baruah <rishithegamecreator@protonmail.ch>
2016-09-19 22:38:56 +0530

    file reading and saving

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>File Handling</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="jas.js"></script>
+</head>
+<body>
+	<div class="col-sm-6">
+		<textarea name="txtarea" id="txtarea" cols="80" rows="30"></textarea>
+	</div>
+	<div class="col-sm-6">
+		<input type="text" id="file" name="file"><br>
+		<button id="gettxt">Get Text</button>
+		<button id="settxt">Set Text</button>
+	</div>
+</body>
+</html>
